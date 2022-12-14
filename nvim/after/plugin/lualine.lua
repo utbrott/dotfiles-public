97a8ec127e4655c8a7663de6f0b57c52b9e6b32f
utbrott <rosinskimk@gmail.com>
2022-12-14 02:04:18 +0100

    feat(lualine): change theme from `auto` to `iceberg_dark`

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,68 +1,68 @@
 local ok, lualine = pcall(require, "lualine")
 if not ok then
-    print("lualine failed")
-    return
+  print("lualine failed")
+  return
 end
 
 lualine.setup({
-    options = {
-        icons_enabled = true,
-        theme = "auto",
-        section_separators = { left = "", right = "" },
-        component_separators = { left = "│", right = "│" },
-        globalstatus = true,
+  options = {
+    icons_enabled = true,
+    theme = "iceberg_dark",
+    section_separators = { left = "", right = "" },
+    component_separators = { left = "│", right = "│" },
+    globalstatus = true,
+  },
+  -- | A | B | C |        | X | Y | Z | --
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = {
+      "branch",
+      {
+        "diff",
+        symbols = { added = "+", modified = "~", removed = "-" },
+      },
     },
-    -- | A | B | C |        | X | Y | Z | --
-    sections = {
-        lualine_a = { "mode" },
-        lualine_b = {
-            "branch",
-            {
-                "diff",
-                symbols = { added = "+", modified = "~", removed = "-" },
-            },
-        },
-        lualine_c = {
-            {
-                "filetype",
-                icon_only = true,
-                padding = { left = 1, right = 0 },
-                separator = { right = "" },
-            },
-            {
-                "filename",
-                file_status = true, -- displays file status
-                path = 0, -- just filename
-            },
-        },
-        lualine_x = {
-            {
-                "diagnostics",
-                sources = { "nvim_diagnostic" },
-                symbols = {
-                    error = " ",
-                    warn = " ",
-                    info = " ",
-                    hint = " ",
-                },
-            },
-        },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
+    lualine_c = {
+      {
+        "filetype",
+        icon_only = true,
+        padding = { left = 1, right = 0 },
+        separator = { right = "" },
+      },
+      {
+        "filename",
+        file_status = true, -- displays file status
+        path = 0, -- just filename
+      },
     },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {
-            {
-                "filename",
-                file_status = true, -- displays file status
-                path = 0, -- just filename
-            },
+    lualine_x = {
+      {
+        "diagnostics",
+        sources = { "nvim_diagnostic" },
+        symbols = {
+          error = " ",
+          warn = " ",
+          info = " ",
+          hint = " ",
         },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
+      },
     },
-    extensions = { "fugitive" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+      {
+        "filename",
+        file_status = true, -- displays file status
+        path = 0, -- just filename
+      },
+    },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  extensions = { "fugitive" },
 })
